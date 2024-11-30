96e7e0c0dbd9c72c4bf25ddc5a3b4b1077ac3ece
Nguyễn Thị Thu Thảo <118255340+thaonguyen16@users.noreply.github.com>
2024-11-30 21:53:21 +0700

    Update axios.ts

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api/v1",
+  baseURL: "https://jobhunt-api-production.up.railway.app/api/v1",
   headers: {
     "Content-Type": "application/json",
   },
